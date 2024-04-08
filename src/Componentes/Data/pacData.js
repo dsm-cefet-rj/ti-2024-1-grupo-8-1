51acac7f87140d4c87f03162a8ec3c42fba0e13c
luccamigo <lucca.amigo@gmail.com>
2024-04-08 14:41:45 -0300

    Edições de Pacientes

--- src/Componentes/Data/pacData.js
+++ src/Componentes/Data/pacData.js
@@ -1,61 +1,137 @@
 const PacData = [
-    {
-      nome: "Payet Bittencourt",
-      telefone: "(11) 98765-4320",
-      cpf: "123.456.789-00",
-      endereco: "Rua Sotero dos reis, 123",
-      cidade: "Rio de Janeiro",
-      pagou : false
-    },
-    
-      {
-        nome: "João da Silva",
-        telefone: "(11) 98765-4321",
-        cpf: "123.456.789-00",
-        endereco: "Rua A, 123",
-        cidade: "São Paulo",
-        pagou : false
-      },
-      {
-        nome: "Maria Souza",
-        telefone: "(21) 99999-8888",
-        cpf: "987.654.321-00",
-        endereco: "Avenida B, 456",
-        cidade: "Rio de Janeiro",
-        pagou : false
-      },
-      {
-        nome: "Carlos Oliveira",
-        telefone: "(31) 77777-2222",
-        cpf: "555.444.333-00",
-        endereco: "Rua C, 789",
-        cidade: "Belo Horizonte",
-        pagou : false
-      },
-      {
-        nome: "Ana Santos",
-        telefone: "(41) 33333-5555",
-        cpf: "222.111.888-00",
-        endereco: "Avenida D, 987",
-        cidade: "Curitiba",
-        pagou : false
-      },
-      {
-        nome: "Pedro Costa",
-        telefone: "(85) 22222-7777",
-        cpf: "666.777.888-00",
-        endereco: "Rua E, 321",
-        cidade: "Fortaleza",
-        pagou : false
-      },
-      {
-        nome: "Fernanda Almeida",
-        telefone: "(61) 44444-1111",
-        cpf: "999.888.777-00",
-        endereco: "Avenida F, 654",
-        cidade: "Brasília",
-        pagou : false
-      }
-    ];
-    
-    export default PacData;
+  {
+    nome: "Payet Bittencourt",
+    telefone: "(11) 98765-4320",
+    cpf: "123.456.789-00",
+    endereco: "Rua Sotero dos reis, 123",
+    cidade: "Rio de Janeiro",
+  },
+  {
+    nome: "João da Silva",
+    telefone: "(11) 98765-4321",
+    cpf: "123.456.789-00",
+    endereco: "Rua A, 123",
+    cidade: "São Paulo",
+  },
+  {
+    nome: "Maria Souza",
+    telefone: "(21) 99999-8888",
+    cpf: "987.654.321-00",
+    endereco: "Avenida B, 456",
+    cidade: "Rio de Janeiro",
+  },
+  {
+    nome: "Carlos Oliveira",
+    telefone: "(31) 77777-2222",
+    cpf: "555.444.333-00",
+    endereco: "Rua C, 789",
+    cidade: "Belo Horizonte",
+  },
+  {
+    nome: "Ana Santos",
+    telefone: "(41) 33333-5555",
+    cpf: "222.111.888-00",
+    endereco: "Avenida D, 987",
+    cidade: "Curitiba",
+  },
+  {
+    nome: "Pedro Costa",
+    telefone: "(85) 22222-7777",
+    cpf: "666.777.888-00",
+    endereco: "Rua E, 321",
+    cidade: "Fortaleza",
+  },
+  {
+    nome: "Fernanda Almeida",
+    telefone: "(61) 44444-1111",
+    cpf: "999.888.777-00",
+    endereco: "Avenida F, 654",
+    cidade: "Brasília",
+  },
+  {
+    nome: "Lucas Santos",
+    telefone: "(11) 55555-5555",
+    cpf: "111.222.333-44",
+    endereco: "Rua G, 789",
+    cidade: "São Paulo",
+  },
+  {
+    nome: "Laura Oliveira",
+    telefone: "(21) 66666-6666",
+    cpf: "555.666.777-88",
+    endereco: "Avenida H, 123",
+    cidade: "Rio de Janeiro",
+  },
+  {
+    nome: "Mariana Lima",
+    telefone: "(31) 98765-4321",
+    cpf: "777.888.999-00",
+    endereco: "Rua I, 456",
+    cidade: "Belo Horizonte",
+  },
+  {
+    nome: "Rafaela Fernandes",
+    telefone: "(41) 55555-5555",
+    cpf: "888.999.000-11",
+    endereco: "Avenida J, 789",
+    cidade: "Curitiba",
+  },
+  {
+    nome: "Anderson Costa",
+    telefone: "(85) 77777-7777",
+    cpf: "111.222.333-44",
+    endereco: "Rua K, 321",
+    cidade: "Fortaleza",
+  },
+  {
+    nome: "Thiago Oliveira",
+    telefone: "(61) 88888-8888",
+    cpf: "222.333.444-55",
+    endereco: "Avenida L, 654",
+    cidade: "Brasília",
+  },
+  {
+    nome: "Isabela Santos",
+    telefone: "(11) 66666-6666",
+    cpf: "333.444.555-66",
+    endereco: "Rua M, 123",
+    cidade: "São Paulo",
+  },
+  {
+    nome: "Juliana Almeida",
+    telefone: "(21) 44444-4444",
+    cpf: "444.555.666-77",
+    endereco: "Avenida N, 456",
+    cidade: "Rio de Janeiro",
+  },
+  {
+    nome: "Marcos Lima",
+    telefone: "(31) 33333-3333",
+    cpf: "555.666.777-88",
+    endereco: "Rua O, 789",
+    cidade: "Belo Horizonte",
+  },
+  {
+    nome: "Patrícia Fernandes",
+    telefone: "(41) 77777-7777",
+    cpf: "777.888.999-00",
+    endereco: "Avenida P, 987",
+    cidade: "Curitiba",
+  },
+  {
+    nome: "Leonardo Costa",
+    telefone: "(85) 55555-5555",
+    cpf: "999.000.111-22",
+    endereco: "Rua Q, 321",
+    cidade: "Fortaleza",
+  },
+  {
+    nome: "Camila Oliveira",
+    telefone: "(61) 99999-9999",
+    cpf: "111.222.333-44",
+    endereco: "Avenida R, 654",
+    cidade: "Brasília",
+  },
+];
+
+export default PacData;
