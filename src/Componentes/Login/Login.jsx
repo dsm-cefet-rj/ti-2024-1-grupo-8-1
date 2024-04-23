c01ea27105fee9d6db605286f0fec04b8049b8c9
luccamigo <lucca.amigo@gmail.com>
2024-04-23 18:56:55 -0300

    tela de login embelezada

--- src/Componentes/Login/Login.jsx
+++ src/Componentes/Login/Login.jsx
@@ -1,58 +1,41 @@
 import React, { useState } from 'react';
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
 import logo from '../Imagens/logo.png'
 import { useDispatch } from 'react-redux';
 import { login } from '../../features/userSlice';
-function Login() {
-    const [email,setEmail] = useState("");
-    const [senha,setSenha] = useState("");
-    const dispatch = useDispatch();
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        dispatch(login({
-            email: email,
-            senha: senha,
-            loggedIn: true,
-    
-        }));
+import "./login.css"
 
-    };
+function Login() {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({
+      email: email,
+      senha: senha,
+      loggedIn: true,
+    }));
+
+  };
 
 
   return (
-    <MDBContainer fluid className="p-3 my-5 h-custom">
-
-      <MDBRow>
-
-        <MDBCol col='10' md='6'>
+    <div>
+      <div>
+        <div className='imagem'>
           <img src={logo} class="img-fluid" alt="Sample image" />
-        </MDBCol>
-
-        <MDBCol col='4' md='6'>
-
-          <div className="d-flex flex-row align-items-center justify-content-center">
-
-          </div>
-          <form onSubmit={(e)=> handleSubmit(e)}>
-        
-          <MDBInput wrapperClass='mb-4'  id='formControlLg' type='email' size="lg" value ={email} onChange={(e)=> setEmail(e.target.value)} placeholder='Digite o seu email'/>
-          <MDBInput wrapperClass='mb-4' id='formControlLg' type='password' size="lg" value={senha} onChange={(e)=> setSenha(e.target.value)} placeholder='Digite sua senha'/>
-
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="!#">Esqueci minha senha</a>
-          </div>
-
-          <div className='text-center text-md-start mt-4 pt-2'>
-            <button className="mb-0 px-5" size='lg' type='submit' > Login</button>
-            <p className="small fw-bold mt-2 pt-1 mb-2">Não tem conta? <a href="#!" className="link-danger">Registrar</a></p>
-          </div>
+        </div>
+        <div className='login'>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input type='email' value={email} className='inputLogin' onChange={(e) => setEmail(e.target.value)} placeholder='Digite o seu email' />
+            <input type='password' value={senha} className='inputLogin' onChange={(e) => setSenha(e.target.value)} placeholder='Digite sua senha' />
+            <div>
+              <button type='submit' className='buttonLogin'> Login</button>
+            </div>
           </form>
-        </MDBCol>
-
-      </MDBRow>
-
-    </MDBContainer>
+        </div>
+      </div>
+    </div>
   );
 }
 
