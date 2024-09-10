b14e29cd26cb5aded3452a7a3976b15786ec675a
Lucca Amigo <142367819+luccamigo@users.noreply.github.com>
2024-09-10 14:28:13 -0300

    isso ta muito zoado

--- src/Componentes/ComponentesPagamentos/Pagamentos.jsx
+++ src/Componentes/ComponentesPagamentos/Pagamentos.jsx
@@ -1,35 +1,55 @@
-import React, { useState } from 'react';
-import PagPaci from './pagPaci'
-import PagData from './pagData'
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-import '../styles.css';
+const Pagamentos = () => {
+  const pagamentos = useSelector((state) => state.listaPagamentos.pagamentos);
+  const compromissos = useSelector((state) => state.listaConsulta.consultas);
+  const [pagamentosPorMes, setPagamentosPorMes] = useState([]);
+  const [mesAtual, setMesAtual] = useState(new Date());
 
-function Pagamentos() {
+  useEffect(() => {
+    pagamentos.map((pagamento) =>{
+        const compromissoDoPagamento = compromissos.find((compromisso) => compromisso._id == pagamento.idConsulta)
+        if(compromissoDoPagamento.dia.getMonth() == mesAtual.getMonth){
+            console.log('vasco')
+        }
+    })
+    // setPagamentosPorMes(pagamentosFiltrados);
+  }, [pagamentos, mesAtual]);
 
-  const [Modo, setModo] = useState('Inicial')
-
-  const handleListarPagamentos = () => {
-    setModo('Inicial')
+  const avancarMes = () => {
+    setMesAtual(new Date(mesAtual.setMonth(mesAtual.getMonth() + 1)));
   };
 
-  const handlePagamentoPorData = () => {
-    setModo('Data')
+  const retrocederMes = () => {
+    setMesAtual(new Date(mesAtual.setMonth(mesAtual.getMonth() - 1)));
   };
 
-  const renderizarConteudo = () => {
-    if (Modo === 'Inicial') {
-      return <PagPaci handlePagamentoPorData={handlePagamentoPorData} />;
-    }
-    else if (Modo === 'Data') {
-      return <PagData handleListarPagamentos={handleListarPagamentos}/>;
-    }
-  }
+  const mesAnoAtual = mesAtual.toLocaleString('pt-BR', { month: 'long', year: 'numeric' });
 
   return (
-    <div className="corpo">
-      {renderizarConteudo()}
+    <div>
+      <div className="controle-mes">
+        <button onClick={retrocederMes}>Anterior</button>
+        <h3>{mesAnoAtual}</h3>
+        <button onClick={avancarMes}>Próximo</button>
+      </div>
+
+      <div className="grid-pagamentos">
+        {pagamentosPorMes.length > 0 ? (
+          pagamentosPorMes.map((pagamento) => (
+            <div key={pagamento._id} className="mini-recibo">
+              <p>Valor Total: R${pagamento.valorTotal.toFixed(2)}</p>
+              <p>Parcela: {pagamento.parcela}</p>
+              <p>Método: {pagamento.metodo}</p>
+            </div>
+          ))
+        ) : (
+          <p>Nenhum pagamento para este mês.</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default Pagamentos;
